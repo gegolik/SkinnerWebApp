367ea7fd238955d4cfa42d51297e4a2e4ec33fbf
Martin <mocchi@certisur.com>
2020-08-28 18:34:29 -0300

    alta y baja de tratamiento corregida

--- src/app/tratamiento-content/tratamiento-content.component.ts
+++ src/app/tratamiento-content/tratamiento-content.component.ts
@@ -2,48 +2,61 @@ import { Component, OnInit } from '@angular/core';
 import { TratamientoService } from '../services/tratamiento.service';
 import { tratamientoObject } from '../Models/tratamiento_model';
 
-
 @Component({
   selector: 'app-tratamiento-content',
   templateUrl: './tratamiento-content.component.html',
-  styleUrls: ['./tratamiento-content.component.css']
+  styleUrls: ['./tratamiento-content.component.css'],
 })
 export class TratamientoContentComponent implements OnInit {
-
-  tratamientos:any
+  tratamientos: any;
   nuevoTratamiento: tratamientoObject = new tratamientoObject();
 
-  lesionItems: Array<any> = [{ name: 'Melanoma', value: 1, defaultSelected: true }, { name: 'Vitiligo', value: 2 }, { name: 'Psoriasis', value: 3 }];
+  lesionItems: Array<any> = [
+    { name: 'Melanoma', value: 1, defaultSelected: true },
+    { name: 'Vitiligo', value: 2 },
+    { name: 'Psoriasis', value: 3 },
+  ];
   selectedValue: string = '';
   listaTratamientos: any;
 
-
-  constructor(public tratservice: TratamientoService) { 
-
-    this.tratservice.getTratamientos().subscribe((tratamientos: any)=>{this.listaTratamientos=tratamientos});
-
+  constructor(public tratservice: TratamientoService) {
+    this.getTratamientos();
   }
 
   ngOnInit(): void {
-
-
-    this.selectedValue = this.lesionItems.filter(a => a.defaultSelected)[0].value;
+    this.selectedValue = this.lesionItems.filter(
+      (a) => a.defaultSelected
+    )[0].value;
     console.log(this.tratamientos);
     itemToRemove: Number;
+  }
 
+  public getTratamientos() {
+    this.tratservice.getTratamientos().subscribe((tratamientos: any) => {
+      this.listaTratamientos = tratamientos;
+    });
   }
 
   public persistirTratamiento(trat: tratamientoObject) {
+    console.log('PERSISTIR');
     const index = this.tratamientos.indexOf(trat);
     if (index != -1) {
-      this.tratservice.deleteTratamiento(this.tratamientos);;
+      this.tratservice.deleteTratamiento(this.tratamientos);
     }
-    this.tratservice.addTratamiento(JSON.parse(JSON.stringify(this.nuevoTratamiento)));
+    this.tratservice
+      .addTratamiento(this.nuevoTratamiento)
+      .subscribe((response) => {
+        console.log(response);
+      });
     this.limpiarTratamiento();
   }
   public agregarTratamiento(trat: tratamientoObject) {
-
-    this.tratservice.addTratamiento(JSON.parse(JSON.stringify(this.nuevoTratamiento)));
+    console.log(this.nuevoTratamiento);
+    this.tratservice
+      .addTratamiento(this.nuevoTratamiento)
+      .subscribe((response) => {
+        this.getTratamientos();
+      });
   }
 
   public limpiarTratamiento() {
@@ -51,15 +64,16 @@ export class TratamientoContentComponent implements OnInit {
   }
 
   public removeTratamiento(tratamiento: any) {
-    if (confirm("¿Desea eliminar el tratamiento?")) {
-      this.tratservice.deleteTratamiento(parseInt(tratamiento.id)).subscribe((cosa: any)=>{console.log(cosa)});
+    if (confirm('¿Desea eliminar el tratamiento?')) {
+      this.tratservice
+        .deleteTratamiento(parseInt(tratamiento.id))
+        .subscribe(() => {
+          this.getTratamientos()
+        });
     }
   }
 
   public assignTratamiento(trat: tratamientoObject) {
     this.nuevoTratamiento = trat;
   }
-
-
-
 }
