148ebde3ec5c00dda8b6e145da77f37ecb133276
Facundo Petrolo <petrolo.f@gmail.com>
2020-10-13 22:52:40 -0300

    Agregado graficos en asignaciones

--- src/app/asignacion/asignacion.component.ts
+++ src/app/asignacion/asignacion.component.ts
@@ -3,6 +3,9 @@ import { AsignacionService } from '../services/asignacion.service';
 import { HistoriaLesionesService } from '../services/historia-lesiones.service';
 import { ActivatedRoute } from '@angular/router';
 import { CookieService } from 'ngx-cookie-service';
+import { ChartType, ChartOptions } from 'chart.js';
+import { SingleDataSet, Label, monkeyPatchChartJsLegend, monkeyPatchChartJsTooltip } from 'ng2-charts';
+
 @Component({
   selector: 'app-asignacion',
   templateUrl: './asignacion.component.html',
@@ -11,11 +14,28 @@ import { CookieService } from 'ngx-cookie-service';
 export class AsignacionComponent implements OnInit {
   respuesta: any;
   respuestaAsignacion:any;
-
+  public pieChartOptions: ChartOptions = {
+    responsive: true,
+    tooltips: {
+      mode: 'label',
+      callbacks: {
+          label: function(tooltipItem, data) {
+              return data['datasets'][0]['data'][tooltipItem['index']] + '%';
+          }
+      }
+  },
+  };
+  public pieChartLabels: Label[] = [['Melanoma'], ['Vitiligo'], ['Lunar'], 'Psoriasis'];
+  public pieChartType: ChartType = 'pie';
+  public pieChartLegend = true;
+  public pieChartPlugins = [];
   constructor(public cookieService:CookieService, public asignacionService: AsignacionService, public lesionService:HistoriaLesionesService,private route:ActivatedRoute) {
     var id=this.route.snapshot.params.id;
     
-    this.asignacionService.getAsignacionesPorUsuario(parseInt(this.cookieService.get('autenticado'))).subscribe((asignacion: any)=>{this.respuesta=asignacion});
+    this.asignacionService.getAsignacionesPorUsuario(parseInt(this.cookieService.get('autenticado'))).subscribe((asignacion: any)=>{this.respuesta=asignacion;       for (let index = 0; index < asignacion.length; index++) {
+      var lesion = asignacion[index];
+      lesion.charData = this.getChartData(lesion.analisis)
+    }});
    
   }
 
@@ -27,4 +47,13 @@ this.asignacionService.updateAsignacionPorId(value,asignacionId).subscribe((asig
 });
 
 }
+public getChartData(resultadoAnalisis:any){
+  var resultadoParse=JSON.parse(resultadoAnalisis);
+  resultadoParse.melanoma= Math.round(resultadoParse.melanoma*100);
+  resultadoParse.vitiligo= Math.round(resultadoParse.vitiligo*100);
+  resultadoParse.lunar= Math.round(resultadoParse.lunar*100);
+  resultadoParse.psoriasis= Math.round(resultadoParse.psoriasis*100);
+  var arrayResultado:SingleDataSet=[resultadoParse.melanoma,resultadoParse.vitiligo,resultadoParse.lunar,resultadoParse.psoriasis]
+ return arrayResultado;
+ }
 }
